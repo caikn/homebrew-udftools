4a6d01ee36ee5072a5cf057bbbedfd2632a61d0f
Kanyin Cai <kanyin.cai@sap.com>
2026-05-23 22:05:36 +0200

    add sha256 to formula

--- Formula/udftools-mac.rb
+++ Formula/udftools-mac.rb
@@ -2,7 +2,7 @@ class UdftoolsMac < Formula
   desc "UDF filesystem utilities for macOS (mkudffs, udfinfo, udflabel)"
   homepage "https://github.com/caikn/homebrew-udftools"
   url "https://github.com/caikn/homebrew-udftools/archive/refs/tags/v2.3.tar.gz"
-  sha256 ""
+  sha256 "cdea247d2a180f368a62e812cf22def715a8bb4df1a94ee01dcd6d923eb6f211"
   license "GPL-2.0-only"
 
   depends_on :macos
